b59be60ac626955c000438f6f75327f5cefe74b6
Raghu Betina <raghu@firstdraft.com>
2017-09-30 18:36:08 +0000

    Automatically associate comment to signed in user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,8 +47,6 @@ class CommentsController < ApplicationController
 
   def update
     @comment = Comment.find(params[:id])
-
-    @comment.user_id = params[:user_id]
     @comment.photo_id = params[:photo_id]
 
     save_status = @comment.save
